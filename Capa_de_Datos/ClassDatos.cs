928c75631c6e74efcdc9714871bc3fd49eaeb632
Arturocruz1 <arturomallquic@gmail.com>
2023-10-23 11:45:19 -0500

    MI SEGUNDO AVANCE EN C# Y SQL

--- Capa_de_Datos/ClassDatos.cs
+++ Capa_de_Datos/ClassDatos.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Capa_de_Datos
 {
